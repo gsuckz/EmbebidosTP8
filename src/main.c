300db0619d80ab745649fb5379fb1a20d9405b98
Jesus Avila <jesus.avila.05@gmail.com>
2023-07-04 03:49:33 -0300

    Agregada Func CheckBot

--- src/main.c
+++ src/main.c
@@ -131,6 +131,113 @@ static void mostrarEnPantalla(Reloj * reloj, ESTADOS estado,uint8_t temp[6]){
     PonchoWriteDisplay(poncho, hhmm); 
     PonchoDrawDisplay(poncho); 
 }
+
+static checkBotones(ESTADOS * estado, ESTADOS * volver, uint8_t temp[6]){
+            
+            if (getEstadoAlarma == ON){
+                if(PonchoBotonAceptar(poncho)) relojSnooze(reloj,5);
+                if(PonchoBotonCancelar(poncho)) setAlarmaEstado(reloj,READY);
+            }
+            switch (*estado){
+            case E_RESET:                      
+            case E_MOSTRAR_HORA: //FALLTHRU
+                relojHorario(reloj,temp);
+                if(isHighF(poncho,2)) {
+                    *volver = *estado;
+                    setTimeOut(3);
+                    *estado = E_ESPERA_MOD_HORARIO;
+                }
+                if(isHighF(poncho,3)) {
+                    *volver = *estado;
+                    setTimeOut(3);
+                    *estado = E_ESPERA_MOD_ALARMA;
+                }
+                if(PonchoBotonAceptar(poncho)) setAlarmaEstado(reloj,READY);
+                if(PonchoBotonCancelar(poncho)) setAlarmaEstado(reloj,OFF);
+            break;case E_ESPERA_MOD_ALARMA:
+                if(!isHighF(poncho,3)) *estado = *volver;
+                if(!TimeOut) {
+                    setTimeOut(30);
+                    getAlarmaHora(reloj,temp);
+                    *estado = E_MOD_ALARMA_MIN;
+                }
+            break;case E_ESPERA_MOD_HORARIO:
+                if(!isHighF(poncho,3)) *estado = *volver;
+                if(!TimeOut) {
+                    setTimeOut(30);
+                    relojHorario(reloj,temp);
+                    *estado = E_MOD_HORARIO_MIN;
+                }
+            break;case E_MOD_ALARMA_MIN:
+                if(PonchoBotonFuncion(poncho,3)){
+                    setTimeOut(30);
+                    incrementarMinutos(temp);
+                }
+                if(PonchoBotonFuncion(poncho,4)){
+                    setTimeOut(30);
+                    decrementarMinutos(temp);
+                }
+                if(PonchoBotonCancelar || !TimeOut){
+                    *estado = *volver;
+                }
+                if(PonchoBotonAceptar(poncho)){
+                    setTimeOut(30);
+                    *estado = E_MOD_ALARMA_HOR;
+                }
+            break;case E_MOD_ALARMA_HOR:
+                if(PonchoBotonFuncion(poncho,3)){
+                    setTimeOut(30);
+                    incrementarHoras(temp);
+                }
+                if(PonchoBotonFuncion(poncho,4)){
+                    setTimeOut(30);
+                    decrementarHoras(temp);
+                }
+                if(PonchoBotonCancelar || !TimeOut){
+                    *estado = *volver;
+                }   
+                if(PonchoBotonAceptar(poncho)){
+                    setTimeOut(30);
+                    setAlarmaHora(reloj,temp);
+                    setAlarmaEstado(reloj,READY);
+                    *estado = E_MOSTRAR_HORA;
+                }                    
+            break;case E_MOD_HORARIO_MIN:
+                if(PonchoBotonFuncion(poncho,3)){
+                    setTimeOut(30);
+                    incrementarMinutos(temp);
+                }
+                if(PonchoBotonFuncion(poncho,4)){
+                    setTimeOut(30);
+                    decrementarMinutos(temp);
+                }
+                if(PonchoBotonCancelar || !TimeOut){
+                    *estado = *volver;
+                }
+                if(PonchoBotonAceptar(poncho)){
+                    setTimeOut(30);
+                    *estado = E_MOD_HORARIO_HOR;
+                }           
+            break;case E_MOD_HORARIO_HOR:
+                if(PonchoBotonFuncion(poncho,3)){
+                    setTimeOut(30);
+                    incrementarHoras(temp);
+                }
+                if(PonchoBotonFuncion(poncho,4)){
+                    setTimeOut(30);
+                    decrementarHoras(temp);
+                }
+                if(PonchoBotonCancelar || !TimeOut){
+                    *estado = *volver;
+                }   
+                if(PonchoBotonAceptar(poncho)){
+                    relojGuardarHora(reloj,temp);
+                    *estado = E_MOSTRAR_HORA;
+                }              
+            break;default:
+            break;
+        }    
+}
 /* === Public function implementation ========================================================= */
 void SysTick_Handler(void){
     if (relojTick(reloj)) {
@@ -146,111 +253,8 @@ int main(void) {
     ESTADOS estado = E_RESET, volver = E_RESET;
     uint8_t temp[6] = {0,0, 0,0 ,0,0};    
     while (1){ ///LAZO PRINCIPAL 
-        mostrarEnPantalla(reloj,estado,temp);   
-        switch (estado){
-            case E_RESET:                      
-            case E_MOSTRAR_HORA: //FALLTHRU
-                relojHorario(reloj,temp);
-                if(isHighF(poncho,2)) {
-                    volver = estado;
-                    setTimeOut(3);
-                    estado = E_ESPERA_MOD_HORARIO;
-                }
-                if(isHighF(poncho,3)) {
-                    volver = estado;
-                    setTimeOut(3);
-                    estado = E_ESPERA_MOD_ALARMA;
-                }
-                if (getEstadoAlarma == ON){
-                    if(PonchoBotonAceptar(poncho)) relojSnooze(reloj,5);
-                    if(PonchoBotonCancelar(poncho)) setAlarmaEstado(reloj,READY);
-                }else{
-                    if(PonchoBotonAceptar(poncho)) setAlarmaEstado(reloj,READY);
-                    if(PonchoBotonCancelar(poncho)) setAlarmaEstado(reloj,OFF);
-                }
-            break;case E_ESPERA_MOD_ALARMA:
-                if(!isHighF(poncho,3)) estado = volver;
-                if(!TimeOut) {
-                    setTimeOut(30);
-                    getAlarmaHora(reloj,temp);
-                    estado = E_MOD_ALARMA_MIN;
-                }
-            break;case E_ESPERA_MOD_HORARIO:
-                if(!isHighF(poncho,3)) estado = volver;
-                if(!TimeOut) {
-                    setTimeOut(30);
-                    relojHorario(reloj,temp);
-                    estado = E_MOD_HORARIO_MIN;
-                }
-            break;case E_MOD_ALARMA_MIN:
-                if(PonchoBotonFuncion(poncho,3)){
-                    setTimeOut(30);
-                    incrementarMinutos(temp);
-                }
-                if(PonchoBotonFuncion(poncho,4)){
-                    setTimeOut(30);
-                    decrementarMinutos(temp);
-                }
-                if(PonchoBotonCancelar || !TimeOut){
-                    estado = volver;
-                }
-                if(PonchoBotonAceptar(poncho)){
-                    setTimeOut(30);
-                    estado = E_MOD_ALARMA_HOR;
-                }
-            break;case E_MOD_ALARMA_HOR:
-                if(PonchoBotonFuncion(poncho,3)){
-                    setTimeOut(30);
-                    incrementarHoras(temp);
-                }
-                if(PonchoBotonFuncion(poncho,4)){
-                    setTimeOut(30);
-                    decrementarHoras(temp);
-                }
-                if(PonchoBotonCancelar || !TimeOut){
-                    estado = volver;
-                }   
-                if(PonchoBotonAceptar(poncho)){
-                    setTimeOut(30);
-                    setAlarmaHora(reloj,temp);
-                    setAlarmaEstado(reloj,READY);
-                    estado = E_MOSTRAR_HORA;
-                }                    
-            break;case E_MOD_HORARIO_MIN:
-                if(PonchoBotonFuncion(poncho,3)){
-                    setTimeOut(30);
-                    incrementarMinutos(temp);
-                }
-                if(PonchoBotonFuncion(poncho,4)){
-                    setTimeOut(30);
-                    decrementarMinutos(temp);
-                }
-                if(PonchoBotonCancelar || !TimeOut){
-                    estado = volver;
-                }
-                if(PonchoBotonAceptar(poncho)){
-                    setTimeOut(30);
-                    estado = E_MOD_HORARIO_HOR;
-                }           
-            break;case E_MOD_HORARIO_HOR:
-                if(PonchoBotonFuncion(poncho,3)){
-                    setTimeOut(30);
-                    incrementarHoras(temp);
-                }
-                if(PonchoBotonFuncion(poncho,4)){
-                    setTimeOut(30);
-                    decrementarHoras(temp);
-                }
-                if(PonchoBotonCancelar || !TimeOut){
-                    estado = volver;
-                }   
-                if(PonchoBotonAceptar(poncho)){
-                    relojGuardarHora(reloj,temp);
-                    estado = E_MOSTRAR_HORA;
-                }              
-            break;default:
-            break;
-        }    
+        mostrarEnPantalla(reloj,estado,temp);  
+        checkBotones(&estado,&volver,temp);         
     }
 }
 
